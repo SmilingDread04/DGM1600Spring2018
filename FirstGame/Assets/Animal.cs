a036811f7a425e0da942a510ec35529aa7555a43
Connor <cbudge9@gmail.com>
2018-01-18 11:15:26 -0700

    Worked on Animal Class, added cube with text that says "Meow."

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	// This declares what start will do
+	void Start () {
+		print("My animal is eating.");
+		print("Now my animal is sleeping.");
+		print("I love my cat.");
+		print("No, I really don't.");
+	}
+
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
