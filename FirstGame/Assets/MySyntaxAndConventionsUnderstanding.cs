5d6a5724d7c01f8287f62736b077e580748dcb14
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-01-23 21:04:39 -0700

    Finished writing for assignment

--- FirstGame/Assets/MySyntaxAndConventionsUnderstanding.cs
+++ FirstGame/Assets/MySyntaxAndConventionsUnderstanding.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class MySyntaxAndConventionsUnderstanding : MonoBehaviour {
 
 	// Use this for initialization
-	// I have little to idea what the hell Syntaxes or Conventions are all about and I probably never will.  Based on my understanding, however, syn
+	// I have little to idea what the hell Syntaxes or Conventions are all about and I probably never will.  Based on my understanding,
+	// however, syntaxes and conventions together are basically the "English sentence structure" of coding, where there are many unique rules that make these
+	// paragraphs of code that make even the simplest of things work.  As for 5 examples to better describe this... No.  Just. No.
 	void Start () {
 		
 	}
