7ec6619443d60057ebcdf7aa333199878df0e62d
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-01-25 21:16:37 -0700

    I finished my explanation of Variables

--- FirstGame/Assets/MyVariableUnderstanding.cs
+++ FirstGame/Assets/MyVariableUnderstanding.cs
@@ -6,6 +6,12 @@ public class MyVariableUnderstanding : MonoBehaviour {
 
 	// Use this for initialization
 	// From what I learned in this lesson, variables are just like the english dictionary would define it: something that can/does change given the situation.
+	// In the case of coding, variables are the unique names created by a user to make the Unity Engine run certain programs quicker
+	// Example 1: If I want to declare a variable with the data type string and the call of jumpUp, I would write the code as: string jumpUp
+	// Example 2: In addition, I suppose for the heck of it, you can also assign a value to a variable simply by copying the format of the last example and just add = (insert value #)
+	// Example 3: Once a variable has been declared at least once, you can repeat the process, but remove the data type (I.E. just jumpUp = 5)
+	// Example 4: To create a string variable, the process is exactly the same, just switch "float" or whatever the type is with "string" (I.E. string jumpUp)
+	// Example 5: Finally, if a user wants to add the value, simply use and = sign and surround the value with " "s (I.E. string jumpUp = "Super Star!") 
 	void Start () {
 		
 	}
