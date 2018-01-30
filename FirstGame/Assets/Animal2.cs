dba252dcc01b77aa70aba71cd5d630e9b9545d1e
Connor <cbudge9@gmail.com>
2018-01-30 11:03:30 -0700

    Edited Animal2 & Added Animal3_Bool

--- FirstGame/Assets/Animal2.cs
+++ FirstGame/Assets/Animal2.cs
@@ -12,6 +12,27 @@ public class Animal2 : MonoBehaviour {
 
 	void OnMouseDown()
 	{
+		if (1 + 2 == 3)
+		{
+			print(3);
+		}
+
+		if (1 + 2 != 6)
+		{
+			print("Not 6");
+		}
+
+		if (foodCount == 0)
+		{
+			print("The pet has no food.");
+		}
+
+		if (foodCount != 0)
+		{
+			print("The " + Name + " has " + foodCount + " food.");
+		}
+	//When an "if" statement is made, the "myUIText" will not work because it is not connected to the "GUI" anymore, and as such can not reference it.
+	//If "NullReferenceException: Object reference not set to an instance of an object" shows up, delete the the .text file, like the one shown below.
 		myUIText.text = "I have a " + Name + ", it has this amount of food: " + foodCount;
 	}
 
---
+++ FirstGame/Assets/Animal3_Bool.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal3_Bool : MonoBehaviour {
+
+	public bool Evil = false;
+	
+	void OnMouseDown ()
+	{
+		if(Evil == true) {
+			print("I will destroy the world!");
+		}
+		if(Evil == false)
+			print("I will save the world!");
+	}
+}
