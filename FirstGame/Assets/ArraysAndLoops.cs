a61db8c2a02d8a7ab5327638ca524c596a70ad65
Connor <cbudge9@gmail.com>
2018-02-15 11:17:57 -0700

    Added Various "TheIncredibles" assets.

--- FirstGame/Assets/ArraysAndLoops.cs
+++ FirstGame/Assets/ArraysAndLoops.cs
@@ -9,46 +9,44 @@ public class ArraysAndLoops : MonoBehaviour {
 // defines a group of certain areas (I.E. list of scores, player order, etc.) with the loop defining how many array elements are used 
 // (I.E. the number in between the brackets used for defining an array), as seen below:
 
-    public string[4] Players;
-    public int[2] Scores;
-    public PowerUp[1] PowerUps;
-    public Player[7] Players;
-    public string[13] Enemies
-    public int[50] Stamina;
-    public int[150] HP;
-    public string[1] Boss;
-    public int[42] Level;
-    public int[600] Time;
+    // public string[4] Players;
+    // public int[2] Scores;
+    // public PowerUp[1] PowerUps;
+    // public Player[7] Players;
+    // public string[13] Enemies
+    // public int[50] Stamina;
+    // public int[150] HP;
+    // public string[1] Boss;
+    // public int[42] Level;
+    // public int[600] Time;
 	    
 // Each string/int/reference, on their own, gives the basic call back to another variable, allowing the user to create multiple command
 // on their own, but after adding the [] symbols, these can be automatically set via Unity's program without having to write every
 // single variable while the number specifies the element, thus avoiding unnecessary infinite repeatings that use all of the game's data.
 
     // Use this for initialization
-    void Start () {
-        foreach (var player in Players) {
-            print(player.PlayerName);
-			print(player.Score);
-			print(player.MPs);
+    //void Start () {
+        //foreach (var player in Players) {
+            //print(player.PlayerName);
+			//print(player.Score);
+			//print(player.MPs);
         }
 
-        foreach (var score in Scores) {
-            if (score >= 1000) {
-                print (score);
-            }
-        }
+        //foreach (var score in Scores) {
+            //if (score >= 1000) {
+                //print (score);
+            //}
+        //}
 
 
 
-        print (Players[0]);
-        print (Players[1]);
-        print (Players[2]);
-        print (Players[3]);
-        print (Players[4]);
-    }
+        //print (Players[0]);
+        //print (Players[1]);
+        //print (Players[2]);
+        //print (Players[3]);
+        //print (Players[4]);
+    //}
 
     // Update is called once per frame
-    void Update () {
-
-    }
-}
+    
+//}
---
+++ FirstGame/Assets/ListsAndLoops.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListsAndLoops : MonoBehaviour 
+{
+
+	public TheIncredibles TheIncredibles;
+	public Text AddMember;
+
+	public string[] PlayerNames;
+	public List<string> PlayerNameList;
+
+	private void OnMouseDown() 
+	{
+		TheIncredibles.FamilyList.Add(AddMember.text);	
+	}
+}
