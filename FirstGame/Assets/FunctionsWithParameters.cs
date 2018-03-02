278f752b86086560aae42c076638793134ad8fd2
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-03-01 21:43:15 -0700

    Updated the "FunctionsWithParameters.cs" file.

--- FirstGame/Assets/FunctionsWithParameters.cs
+++ FirstGame/Assets/FunctionsWithParameters.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FunctionsWithParameters : MonoBehaviour 
+public class FunctionsWithParameters : MonoBehaviour
+
+// Parameter-based functions, as I would have expected, are coding strings that work uniquely with different parts of a script, whether
+// they be from the coding software's index of commands or simple words implemented by the coder for the sake of filling placeholders,
+// such as with the unique coding strings listed down below:
+
 {
 	public int b;
 	public int Health = 20;
@@ -13,14 +18,35 @@ public class FunctionsWithParameters : MonoBehaviour
 
 	void OnTriggerEnter(Collider other)
 	{
-		print("Do something");
+		print(other + "took damage.");
 		print(other);
 
 		AddNumbers(14, 2);
 		AddHealth(25);
 		PlayerData("Bob");
 	}
-
+	
+	void OnTriggerExit(Collider other)
+	{
+		print(other + "is scarred for life...");
+		print(Collider + "is a monster!")
+	}
+	
+	void OnBecameVisible ()
+	{
+		enabled = true
+		print("Who's that Pokemon?");
+		print("A: Ghastly")
+		print("B: Rotom")
+		print("C: Magikarp)
+	}
+	
+	void OnBecameInvisible ()
+	{
+		enabled = false
+		print("Iiiit'sss... Not there...  What the hell?")
+	}
+	
 	void AddNumbers(int a, int b)
 	{
 		print(a += b);	
@@ -35,6 +61,10 @@ public class FunctionsWithParameters : MonoBehaviour
 	{
 		Health += newHealth;
 	}
+	
+	void OnFailedToConnect(NetworkConnectionError error) {
+        Debug.Log("...You're a " + other + ", you can't play MOBAs: " + error);
+    }
 
 }
 
