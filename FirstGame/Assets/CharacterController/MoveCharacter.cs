d4836fd0b3ddd4bcf59777a0babc9525100db982
Connor <cbudge9@gmail.com>
2018-03-29 11:09:17 -0600

    Added MovePattern.cs file & edited MoveCharacter.cs file.

--- FirstGame/Assets/CharacterController/MoveCharacter.cs
+++ FirstGame/Assets/CharacterController/MoveCharacter.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Things in "//" are original components that don't work w/ MovePattern Script added, non-// are for MovePattern only
 public class MoveCharacter : MonoBehaviour {
 
-	public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
+	// public float speed = 6.0F;
+    // public float jumpSpeed = 8.0F;
+    // public float gravity = 20.0F;
+    public MovePattern MovePattern;
     private CharacterController controller;
 
     private void Start()
@@ -14,19 +15,22 @@ public class MoveCharacter : MonoBehaviour {
         controller = GetComponent<CharacterController>();
     }
 
-    private Vector3 moveDirection = Vector3.zero;
+    // private Vector3 moveDirection = Vector3.zero;
     void Update() {
-        if (controller.isGrounded) {        
-            moveDirection.x = Input.GetAxis("Horizontal");
-            moveDirection.y = 0;
-            moveDirection.z = Input.GetAxis("Vertical");
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
+        MovePattern.Move(controller, transform);
+        
+        // if (controller.isGrounded) {        
+            // moveDirection.x = Input.GetAxis("Horizontal");
+            // moveDirection.y = 0;
+            // moveDirection.z = Input.GetAxis("Vertical");
+
+            // moveDirection = transform.TransformDirection(moveDirection);
+            // moveDirection *= speed;
+            // if (Input.GetButton("Jump"))
+                // moveDirection.y = jumpSpeed;
             
-        }
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        // }
+        // moveDirection.y -= gravity * Time.deltaTime;
+        // controller.Move(moveDirection * Time.deltaTime);
     }
 }
---
+++ FirstGame/Assets/CharacterController/MovePattern.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MovePatter")]
+public class MovePattern : MonoBehaviour {
+
+	public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+
+	public void Move(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded) {        
+        	moveDirection.x = Input.GetAxis("Horizontal");
+        	moveDirection.y = 0;
+        	moveDirection.z = Input.GetAxis("Vertical");
+            
+        	moveDirection = transform.TransformDirection(moveDirection);
+        	moveDirection *= speed;
+        	if (Input.GetButton("Jump"))
+            	moveDirection.y = jumpSpeed;
+            
+        }
+    	moveDirection.y -= gravity * Time.deltaTime;
+    	controller.Move(moveDirection * Time.deltaTime);
+	}
+}
