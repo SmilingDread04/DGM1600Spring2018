ce9654f77b77a121627d69e517f8085e178a826e
Connor <cbudge9@gmail.com>
2018-04-05 09:01:07 -0600

    Created New Project For Final Game & Necessary C# Scripts to Succeed.

---
+++ FinalGameProject/Assets/FinalGameInstructions.cs
@@ -0,0 +1,52 @@
+﻿// Create a Game IE Sides-scroller, Top-down or Determined by the instructor (demonstrated by instructor)
+// Add more features to the game (Demonstrated by instructor)
+// Add gameplay (Demonstrated by instructor)
+// Add advanced gameplay (Demonstrated by instructor)
+
+// The project must contain:
+
+// Use of GitHub 40+ commits
+// Use of GitKraken
+// Scripts as Behavior Components
+// Use of Scriptable Objects
+// Conventions and Syntax
+// Variables (Fields and Parameters)
+// Arrays
+// Data Types
+// Void Functions
+// Return Functions
+// Functions with Parameters
+// Classes
+// Scope and Access Modifiers
+// If Statements
+// Else Clause
+// Switch Statements
+// Enumerations 
+// For Loops
+// Foreach Loops
+// While Loops
+
+// Unity Component Requirements:
+
+// Character Controller
+// Triggers/Colliders
+// Navmesh
+// UI
+// Particles 
+// Prefabs
+
+// Game Mechanics Requirements: 
+
+// PowerUp/Downs
+// Health
+// Character Movement
+// State Machine
+// Instructor Choice
+
+// Grading Criteria:
+
+// Work ethic
+// Problem-solving
+// Completion of the project with all requirements
+
+//Submit your github.com URL with the project to Canvas
