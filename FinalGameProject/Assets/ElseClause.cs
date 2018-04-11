19814b7280500259a225aa321deb2e07a8fdd477
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-04-11 14:23:54 -0600

    Commit 3/40: Updated ElseClause.cs w/ Properties.

--- FinalGameProject/Assets/ElseClause.cs
+++ FinalGameProject/Assets/ElseClause.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ElseClause : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+//Makes the different colors act in very specific ways when colliding with the object.
+public class ElseClause : MonoBehaviour 
+{
+  void OnCollisionEnter (Collision col)
+	{
+   	if(col.gameObject.name == "Red")
+   		{
+     		Destroy(col.gameObject);
+		}
+  	  
+	else if(col.gameObject.name == "Green")
+		{
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		}
 		
+	else if(col.gameObject.name == "Blue")
+		{
+		GetComponent<RigidBody2D> ().gravityscale = 0;
+		GetComponent<BoxCollider2D>. ()IsTrigger = true; //Might be able to remove with no ramifications
+		GetComponent<>SpriteRenderer> ().color = new Color (1, 1, 1, .5f);
+		}
+		
+	//else if(col.gameObject.name == "Invisible"
+		//{
+		//Application.LoadLevel();	
+		//}
 	}
 }
