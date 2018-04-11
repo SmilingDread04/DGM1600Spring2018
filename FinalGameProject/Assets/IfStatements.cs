1bf1388edca200362a0ce9c1b7004b9c307ec044
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-04-11 13:59:14 -0600

    Commit 2/40: Updated IfStatements.cs w/ Collision.

--- FinalGameProject/Assets/IfStatements.cs
+++ FinalGameProject/Assets/IfStatements.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class IfStatements : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+//Changes the color of the Player Object when standing on a specific tile.
+public class IfStatements : MonoBehaviour 
+{
+  void OnCollisionEnter (Collision col)
+	{
+   	if(col.gameObject.name == "Red")
+   		{
+     		col.gameObject.GetComponent<Renderer>().material.color = Color.Red;
+		}
+  	  
+	else if(col.gameObject.name == "Green")
+		{
+		col.gameObject.GetComponent<Renderer>().material.color = Color.Green;
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	else if(col.gameObject.name == "Blue")
+		{
+		col.gameObject.GetComponent<Renderer>().material.color = Color.Blue;
+		}
 	}
 }
