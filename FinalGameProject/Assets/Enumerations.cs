987f30ec37e1205af001abd2d431ddf699d15ef7
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-04-13 23:25:52 -0600

    Commit 7/40: Added Difficulty Enumeration.

--- FinalGameProject/Assets/Enumerations.cs
+++ FinalGameProject/Assets/Enumerations.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Enumerations : MonoBehaviour {
 
+public enum Difficulty {Easy, Medium, Hard};
 	// Use this for initialization
 	void Start () {
-		
+		Difficulty myDifficulty
+			
+		myDifficulty = Difficulty.Easy
 	}
 	
 	// Update is called once per frame
