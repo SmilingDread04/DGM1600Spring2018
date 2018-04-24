28f5b9396dcf003efd2dc7066d9630493c394afd
Connor <cbudge9@gmail.com>
2018-04-24 08:58:58 -0600

    Commit 30/40: Added the "TilePowers.cs" (A.K.A. Power Ups & Downs) file.

---
+++ FinalGameProject/Assets/New/Scripts/TilePowers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePowers : MonoBehaviour {
+
+    public MoveCharacter stats;
+
+    private void OnDestroy()
+    {
+        {
+            if (gameObject.name == "Red")
+            {
+                stats.gravity = 1.0F;  
+            }
+
+            else if (gameObject.name == "Green")
+            {
+                stats.speed = 12.0F;
+            }
+
+            else if (gameObject.name == "Blue")
+            {
+                stats.jumpSpeed = 16.0F;
+            }
+        }
+    }
+
+}
