3e09053c93b085c56a8fdda906fb5ead7c817fb5
Connor Budge <35269787+SmilingDread04@users.noreply.github.com>
2018-05-01 06:49:31 -0600

    Commit 46/40: Rendered "BasicAI.cs" null and void.

--- FinalGameProject/Assets/New/Scripts/BasicAI.cs
+++ FinalGameProject/Assets/New/Scripts/BasicAI.cs
@@ -5,12 +5,11 @@ using UnityEngine.AI;
 
 public class BasicAI : MonoBehaviour {
 
-	public NavMeshAgent Agent;
-	public Transform Player;
+	//public NavMeshAgent Agent;
+	//public Transform Player;
 
-    // Use this for initialization
-    void Start () {
-		Agent = GetComponent<NavMeshAgent>();
-		Agent.destination = Player.position;
-	}
-}
+    //void Start () {
+		//Agent = GetComponent<NavMeshAgent>();
+		//Agent.destination = Player.position;
+	//}
+//}
